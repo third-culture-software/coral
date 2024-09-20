3726e1e796204b1109383f4dc7806040db3c87c3
Jonathan Niles <jonathanwniles@gmail.com>
2024-09-20 10:25:21 +0100

    fix: update tests to reflect Uint8Array
    Puppeteer silently changed their API to return a Uint8Array instead of
    a buffer causing test to fail.  Here, I am simply updating the tests.
    If we wanted to keep the old API, we could call new Buffer() on the
    output of the render function, but I believe this is likely unnecessary.

--- test/pdf.spec.js
+++ test/pdf.spec.js
@@ -6,7 +6,9 @@ const render = require('..');
 
 test('renders a buffer', async (t) => {
   const rendered = await render('<html></html>');
-  t.true(Buffer.isBuffer(rendered), 'Did not produce a buffer');
+
+  t.true(rendered instanceof Uint8Array, 'Did not produce a Uint8Array');
+  // filetype should still detect this correctly using the fromBuffer
   const type = await fileType.fromBuffer(rendered);
   t.is(type.mime, 'application/pdf');
 });
@@ -15,7 +17,7 @@ test('renders a PDF buffer from an html file', async (t) => {
   const file = path.join(__dirname, './fixtures/simple.html');
   const template = await fs.promises.readFile(file, 'utf8');
   const rendered = await render(template);
-  t.true(Buffer.isBuffer(rendered), 'Did not produce a buffer');
+  t.true(rendered instanceof Uint8Array, 'Did not produce a Uint8Array');
   const type = await fileType.fromBuffer(rendered);
   t.is(type.mime, 'application/pdf');
 });
